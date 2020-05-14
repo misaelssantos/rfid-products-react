ff9d2be1ead37046a3eff9729a55dcb10a626b02
Misael da Silva Santos <misael.santos@serpro.gov.br>
2020-05-14 11:55:03 -0300

    correção de warnings html

--- src/components/products.js
+++ src/components/products.js
@@ -5,9 +5,9 @@ const Products = ({ products }) => {
     <div>
         <center><h1>Lista de Compras</h1></center>
         {products.map((product) => (
-        <div class="card">
-            <div class="card-body">
-            <h5 class="card-title">{product.name}</h5>
+        <div className="card" key={product.tag}>
+            <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
             </div>
         </div>
         ))}
