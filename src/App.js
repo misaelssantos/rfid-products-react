aa01462b9c2a8db85c52c51570cb8a4be0c131fd
Misael da Silva Santos <misael.santos@serpro.gov.br>
2020-05-16 19:52:32 -0300

    implementação do refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Products from './components/products';
-
+import { properties } from './properties.js';
 import './App.css';
 
 class App extends Component {
@@ -9,18 +9,31 @@ class App extends Component {
     products: []
   }
 
+  callLista() {
+    fetch(properties.LISTA_REST_SERVICE)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ products: data.products })
+      })
+      .catch(console.log);
+  }
+
   componentDidMount() {
-    fetch('http://localhost:8000/api/lista')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ products: data.products })
-    })
-    .catch(console.log)
+    this.callLista();
+    this.interval = setInterval(() => this.setState({ time: Date.now() }), 2000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  componentDidUpdate() {
+    this.callLista();
   }
 
   render() {
     return (
-      <Products products={this.state.products} /> 
+      <Products products={this.state.products}/> 
     );
   }
 }
---
+++ src/properties.js
@@ -0,0 +1,6 @@
+export const properties = {
+    LISTA_REST_SERVICE: "http://192.168.0.17:8000/api/lista",
+    MQTT_HOST: "192.168.0.17",
+    MQTT_USERNAME: "",
+    MQTT_PASSWORD: ""
+};
